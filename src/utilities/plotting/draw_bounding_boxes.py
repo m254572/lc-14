3a69803a18deca78ca5cfddb5c7704c6aded5ab2
cdjellen <cdjellen@gmail.com>
2025-01-21 21:20:34 -0800

    update with a baseline notebook

--- src/utilities/plotting/draw_bounding_boxes.py
+++ src/utilities/plotting/draw_bounding_boxes.py
@@ -5,6 +5,28 @@ import pandas as pd
 from IPython.display import Image, display
 
 
+def bounds_from_geometry(geometry: List[Tuple[int, int]]) -> Union[Tuple[int, int, int, int], None]:
+  """
+  Obtain the coordinates of the bounding box from a GEOJson geometry.
+
+  Args:
+    geometry (list): The GEOJson geometry coordinates
+
+  Returns:
+    x_min, y_min, x_max, y_max (int, int, int, int): The coordinates of the bounding box.
+  """
+  if not geometry:
+    return None
+
+  # Unpack coordinates, ensuring they are integers
+  x_coords, y_coords = zip(*((int(x), int(y)) for x, y in geometry))
+
+  # Calculate min/max values
+  x_min, x_max = min(x_coords), max(x_coords)
+  y_min, y_max = min(y_coords), max(y_coords)
+
+  return x_min, y_min, x_max, y_max
+
 def draw_bounding_boxes(image_filepath, df: Optional[pd.DataFrame] = None, prediction: Optional[Dict[str, Union["np.ndarray", "torch.Tensor"]]] = None):
   """
   Displays an image with bounding boxes from a Pandas DataFrame.
@@ -39,7 +61,7 @@ def draw_bounding_boxes(image_filepath, df: Optional[pd.DataFrame] = None, predi
       label = row["class"]
       geometry = row["geometry"]
 
-      x_min, y_min, x_max, y_max = _bounds_from_geometry(geometry=geometry)
+      x_min, y_min, x_max, y_max = bounds_from_geometry(geometry=geometry)
 
       # Add rectangle
       cv2.rectangle(img, (x_min, y_min), (x_max, y_max), (0, 0, 200), 2)  # Red rectangle
@@ -73,28 +95,6 @@ def draw_bounding_boxes(image_filepath, df: Optional[pd.DataFrame] = None, predi
       # Add label text
       label_text = f"{label}: {score:.2f}"
       cv2.putText(img, label_text, (x_min, y_min - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 0), 2)
-
-  def _bounds_from_geometry(geometry: List[Tuple[int, int]]) -> Union[Tuple[int, int, int, int], None]:
-    """
-    Obtain the coordinates of the bounding box from a GEOJson geometry.
-
-    Args:
-      geometry (list): The GEOJson geometry coordinates
-
-    Returns:
-      x_min, y_min, x_max, y_max (int, int, int, int): The coordinates of the bounding box.
-    """
-    if not geometry:
-      return None
-
-    # Unpack coordinates, ensuring they are integers
-    x_coords, y_coords = zip(*((int(x), int(y)) for x, y in geometry))
-
-    # Calculate min/max values
-    x_min, x_max = min(x_coords), max(x_coords)
-    y_min, y_max = min(y_coords), max(y_coords)
-
-    return x_min, y_min, x_max, y_max
 
   # Load the image
   img = cv2.imread(image_filepath)
